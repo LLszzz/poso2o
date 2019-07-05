2c0889efdcb3f61bebc70ea88395b3f523263a74
LLszzz <912640235@qq.com>
2019-07-05 17:44:35 +0800

    zzz

--- src/views/Video.vue
+++ src/views/Video.vue
@@ -2,7 +2,7 @@
 	<!--播放页面-->
 	<div class="Video">
 		<div class="live" @click="isShop=false">
-			<video :width="width" :height="height"  autoplay="autoplay">
+			<video :width="width" :height="height"  webkit-playsinline="true" x5-video-player-type="h5" x5-video-player-fullscreen="true" playsinline="true"  autoplay="autoplay">
 				<source src="@/assets/video/video.mp4" type="video/mp4"></source>
 				<source src="myvideo.ogv" type="video/ogg"></source>
 				<source src="myvideo.webm" type="video/webm"></source>
@@ -12,6 +12,7 @@
 				</object>
 				当前浏览器不支持 video直接播放，点击这里下载视频： <a href="myvideo.webm">下载视频</a>
 			</video>
+			<!--<ali-player ></ali-player>-->
 		</div>
 		<div class="top">
 			<div>
@@ -94,7 +95,7 @@
 					<p>牛油果吊带连衣裙两件套....</p>
 					<div>
 						<span>￥188</span>
-						<i class="iconfont icon-tubiaolunkuo-"></i>
+						<i class="iconfont icon-tubiaolunkuo-" @click="isCommodity=true"></i>
 					</div>
 				</div>
 			</div>
@@ -159,17 +160,81 @@
 				</div>
 			</div>
 		</div>
+		<!--商品-->
+		<div class="commodity" v-if="isCommodity">
+			<div>
+				<div>
+					<div><img src="../assets/img/5.jpg"/></div>
+					<div>
+						<div @click="isCommodity=false">
+							<i class="iconfont icon-fork"></i>
+						</div>
+						<span>￥188</span>
+						<p>库存1000件</p>
+						<h5>选择尺码,颜色分类</h5>
+					</div>
+				</div>
+				<div class="commodity_sel">
+					<h5>尺码</h5>
+					<ul>
+						<li>S</li>
+						<li>M</li>
+						<li>L</li>
+					</ul>
+				</div>
+				<div class="commodity_sel">
+					<h5>颜色分类</h5>
+					<ul>
+						<li>
+							<div><img src="../assets/img/6.jpg"/></div>
+							<span>黑色</span>
+						</li>
+						<li>
+							<div><img src="../assets/img/6.jpg"/></div>
+							<span>红色</span>
+						</li>
+					</ul>
+				</div>
+				<div class="quantity">
+					<h5>购买数量</h5>
+					<div>
+						<i @click="Quantity>1?Quantity--:Quantity=1">-</i>
+						<span>{{Quantity}}</span>
+						<i @click="Quantity++">+</i>
+					</div>
+				</div>
+				<div class="commodity_bottom">
+					<div>
+						<div>
+							<i class="iconfont icon-aixin"></i>
+							<p>收藏</p>
+						</div>
+					</div>
+					<div>
+						<div>
+							<i class="iconfont icon-liaotian2"></i>
+							<p>联系</p>
+						</div>
+					</div>
+					<span style="background: #FF5101;">加入购物车</span>
+					<span style="background: red;">立即购买</span>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
 <script>
+	import VueAliplayer from 'vue-aliplayer'
 	export default {
 		data(){
 			return{
 				width:0,
 				height:0,
 				placeholder:"跟主播聊什么?",
-				isShop:false
+				isShop:false,
+				isCommodity:false,
+				Quantity:1
 			}
 		},
 		methods:{
@@ -183,13 +248,16 @@
 		},
 		mounted(){
 			this.getSize();
+		},
+		components:{
+			'ali-player': VueAliplayer
 		}
 		
 	}
 </script>
 
 <style scoped="scoped" lang="scss">
-	@import url("//at.alicdn.com/t/font_1275272_uoha0p3daat.css");
+	@import url("//at.alicdn.com/t/font_1275272_pr9tbim6nca.css");
 	.isShop{
 		transform: translateY(-55vh);
 	}
@@ -243,6 +311,175 @@
 			}
 		}
 	}
+	.commodity_bottom{
+		width: 100vw;
+		height: 8vh;
+		display: flex;
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		background: #f5f5f5;
+		align-items: center;
+		>div{
+			width: 20vw;
+			display: inline-flex;
+			height: 8vh;
+			align-items: center;
+			justify-content: center;
+			align-items: center;
+			>div{
+				text-align: center;
+				>i{
+					font-size: 20px;
+				}
+				>p{
+					text-align: center;
+					width: 100%;
+					font-size: 12px;
+				}
+			}
+			
+		}
+		>span{
+			width: 30vw;
+			display: inline-block;
+			height: 8vh;
+			line-height: 8vh;
+			text-align: center;
+			color: white;
+		}
+	}
+	.quantity{
+		box-sizing: border-box;
+		padding: 2vh 4vw;
+		width: 96%;
+		margin: 0 auto;
+		border-bottom: 1px solid #f5f5f5;
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		>div{
+			>i,>span{
+				display: inline-block;
+				background: #f5f5f5;
+				width: 8vw;
+				height:8vw;
+				text-align: center;
+				line-height: 8vw;
+				font-style: normal;
+				border-radius:1vw ;
+			}
+			>span{
+				margin: 0 1vw;
+				border-radius:0 ;
+			}
+		}
+	}
+	.commodity{
+		width: 100vw;
+		height: 100vh;
+		background: rgba(0,0,0,0.6);
+		position: absolute;
+		top: 0;
+		left: 0;
+		>div{
+			position: relative;
+			height:70vh;
+			width: 100%;
+			background: white;
+			position: absolute;
+			left: 0;
+			bottom: 0;
+			>div:nth-of-type(1){
+				box-sizing: border-box;
+				padding: 2vh 4vw;
+				display: flex;
+				width: 100%;
+				>div:nth-of-type(1){
+					width:34vw;
+					height:34vw;
+					overflow: hidden;
+					border-radius:2vw;
+
+					>img{
+						width: 100%;
+						height: 100%;
+					}
+				}
+				>div:nth-of-type(2){
+					width: 58vw;
+					box-sizing: border-box;
+					padding:1vh 0;
+					padding-left: 4vw;
+					>div{
+						text-align: right;
+						>i{
+							display: inline-block;
+							width:5vw;
+							height:5vw;
+							border: 1px solid black;
+							border-radius:50% ;
+							font-size: 12px;
+							text-align: center;
+							line-height: 5vw;
+						}
+					}
+					>span{
+						color: #ff5101;
+						font-weight: bold;
+					}
+					>p{
+						font-size: 12px;
+						color: #b2b2b2;
+						margin: 2vh 0;
+					}
+					>h5{
+						font-weight: 100;
+					}
+				}
+			}
+			.commodity_sel{
+				box-sizing: border-box;
+				padding: 2vh 4vw;
+				width: 96%;
+				margin: 0 auto;
+				border-bottom: 1px solid #f5f5f5;
+				>ul{
+					width: 100%;
+					display: flex;
+					margin-top: 2vh;
+					>li{
+						margin-right:4vw ;
+						min-width: 14vw;
+						min-height: 5vh;
+						text-align: center;
+						padding: 0.5vh 4vw;
+						background: #f5f5f5;
+						border-radius:2vw ;
+						display: flex;
+						justify-content: center;
+						align-items: center;
+						>div{
+							width: 6vw;
+							height: 6vw;
+							margin-right: 2vw;
+							overflow: hidden;
+							border-radius:1vw ;
+							>img{
+								width: 100%;
+								height: 100%;
+							}
+						}
+						>span{
+							font-size: 12px;
+						}
+					}
+				}	
+			}
+		}
+	}
+	
+	
 	.shop_commodity{
 		display: flex;
 		justify-content: space-between;
